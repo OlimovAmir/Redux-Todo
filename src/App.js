d3cd75854bd507f358b7cf55a867051acc51e426
olimov Amir <olimov.amir@mail.ru>
2023-11-12 17:24:42 +0300

    add function delete

--- src/App.js
+++ src/App.js
@@ -6,29 +6,44 @@ function App() {
 
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
-  const ID = uuidv4();
-  const addTodo = ()=>{
-    if (text.trim().length) {
-      
-      setText([
+  const id = uuidv4();
+  const addTodo = () => {
+    if (text.trim().length > 0) {
+      setTodos([
         ...todos,
         {
-          id: ID,
-        } 
-
+          id: id,
+          text: text,
+          completed: false,
+        }
       ])
       setText('');
     }
-
     
+
+  }
+
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !== todoId));
   }
 
   return (
     <div className="App">
       <label>
-        <input value={text} onChange={(e)=> setText(e.target.value)} />
+        <input value={text} onChange={(e) => setText(e.target.value)} />
         <button onClick={addTodo} >Add Todo</button>
       </label>
+
+      <ul>
+        {
+          todos.map(todo => <li key={todo.id}>
+            <input type='checkbox' />
+            <span>{todo.text}</span>
+            <span onClick={() => removeTodo(todo.id)} className='delete' >&times;</span>
+          </li>)
+        }
+      </ul>
+
     </div>
   );
 }
